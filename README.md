5f2de0efa357e93f0ee391f5b2fa4d67b1e06909
Sven-Bo <jubbel3@gmx.net>
2021-09-06 09:53:01 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+
+# Integrate A Contact Form Inside Your Streamlit App! ✉
+
+Integrate a contact form inside your Streamlit app using the free service ‘Form Submit’.
+► Form Submit: https://formsubmit.co/
+► CSS Code: https://www.w3schools.com/howto/howto_css_contact_form.asp
+
+
+## Screenshot
+
+![Contact Form Screenshot](https://www.screencast.com/t/1lnq7qY0)
+
+
+## Author
+
+- Sven from Coding Is Fun
+- YouTube: https://youtube.com/c/CodingIsFun
+- Website: https://pythonandvba.com
+
+
+
+## Feedback
+
+If you have any feedback, please reach out to me at contact@pythonandvba.com
+
+
+![Logo](https://content.screencast.com/users/jubbel3/folders/Snagit/media/c42ea34b-4057-4754-96b0-e8e05c866afb/08.18.2021-19.56.png)
+
---
+++ contact_form.py
@@ -0,0 +1,24 @@
+import streamlit as st  # pip install streamlit
+
+st.header(":mailbox: Get In Touch With Me!")
+
+
+contact_form = """
+<form action="https://formsubmit.co/YOUREMAIL@EMAIL.COM" method="POST">
+     <input type="hidden" name="_captcha" value="false">
+     <input type="text" name="name" placeholder="Your name" required>
+     <input type="email" name="email" placeholder="Your email" required>
+     <textarea name="message" placeholder="Your message here"></textarea>
+     <button type="submit">Send</button>
+</form>
+"""
+
+st.markdown(contact_form, unsafe_allow_html=True)
+
+# Use Local CSS File
+def local_css(file_name):
+    with open(file_name) as f:
+        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+
+
+local_css("style/style.css")
